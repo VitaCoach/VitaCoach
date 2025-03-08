ebfcc1b2d98746ea913d9a2a7b84f44a30488464
m2nsp <mini071663@gmail.com>
2025-03-09 00:56:04 +0900

    feat: 구독관련로직추가
    이슈: #4
    - 사용자의 구독 업데이트
    - 사용자의 구독 상태 반환

---
+++ backend/controllers/subscribeController.js
@@ -0,0 +1,54 @@
+const userService = require('../services/userService');
+
+/**
+ * 구독상태 확인하기
+ */
+const getSubscriptionStatus = (req, res, next) => {
+    const userId = req.user.userId;
+
+    if(!userId){     
+        const error = new Error('User not logged in');
+        error.statusCode = 401;
+        return next(error);
+    }
+
+    //사용자 구독 상태 가져오기
+    const userSubscription = userService.getUserSubscription(userId);
+
+    if(userSubscription === 'FREE'){
+        return res.status(200).json({ redirectTo: '/subscribe'});   //구독페이지 URL
+    }else{
+        return res.status(200).json({redirectTo: '/change-plan'});  //구독 변경 페이지 URL
+    }
+};
+
+
+/**
+ * 플랜 선택하기
+ */
+const selectPlan = (req, res, next) => {
+    const plan = req.body.plan;
+    try{
+        //1. 로그인 정보 가져오기
+        const userId = req.user.userId;
+
+        //로그인 안 되어 있을 경우
+        if(!userId){     
+            const error = new Error('User not logged in');
+            error.statusCode = 401;
+            return next(error);
+        }
+
+        //2. db에서 해당 사용자 찾아서 플랜 업데이트
+        userService.updatePlan(userId, plan);
+    
+        res.status(200).json({ message: 'Plan Updated'});
+    }catch(error){
+        next(error);
+    }
+};
+
+module.exports = {
+    getSubscriptionStatus,
+    selectPlan
+};
--- backend/routes/path.js
+++ backend/routes/path.js
@@ -1,9 +1,13 @@
 const express = require('express');
 
+const verifyToken = require('../middleware/authMiddleware');
+
 const authRoutes = require('./authRoutes');
+const subscribeRoutes = require('./subscribeRoutes');
 
 const router = express.Router();
 
 router.use("/auth", authRoutes);
+router.use("/sub", verifyToken, subscribeRoutes);
 
 module.exports = router;
