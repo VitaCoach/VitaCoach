41aa9653d321063b3494ebb25533ae3a593a8470
m2nsp <mini071663@gmail.com>
2025-03-09 01:54:00 +0900

    feat: 상담조회
    이슈: #2
    - 타입별 전문가 조회
    - 전문가 랜덤 프로필 이미지 추가

--- backend/routes/path.js
+++ backend/routes/path.js
@@ -4,10 +4,12 @@ const verifyToken = require('../middleware/authMiddleware');
 
 const authRoutes = require('./authRoutes');
 const subscribeRoutes = require('./subscribeRoutes');
+const counselRoutes = require('./counselRoutes');
 
 const router = express.Router();
 
 router.use("/auth", authRoutes);
 router.use("/sub", verifyToken, subscribeRoutes);
+router.use("/counsel", verifyToken, counselRoutes);
 
 module.exports = router;
