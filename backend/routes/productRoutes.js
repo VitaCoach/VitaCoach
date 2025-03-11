9b3bbd6c73c74767202959bdbdfa8c5415d5da28
m2nsp <mini071663@gmail.com>
2025-03-11 18:46:47 +0900

    feat: 장바구니, 결제 관련 로직추가
    이슈: #4
    - 장바구니 기능
    - 결제 등록/취소 기능
    - 결제 상품 목록

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -9,7 +9,22 @@ router.get('/:category', productController.getProductList);
 //제품 상세
 router.get('/detail/:productId', productController.getProductInfo);
 
-//제품 구매
+//제품 구매 - 하나만 바로구매
 router.post('/buy', productController.buyProduct);
+
+//제품 구매 취소 - 결제 후 취소
+router.post('/cancel', productController.cancelPayment);
+
+//구매 제품 목록보기
+router.get('/purchasedProducts', productController.getPurchasedList);
+
+//장바구니 추가
+router.post('/cart', productController.addCart);
+
+//장바구니 업데이트
+router.patch('/updateCart', productController.updateCart);
+
+//장바구니 삭제
+router.delete('/cart/:productId', productController.deleteCart);
 
 module.exports = router;
--- backend/services/productService.js
+++ backend/services/productService.js
@@ -1,4 +1,4 @@
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient, OrderStatus } = require('@prisma/client'); 
 const prisma = new PrismaClient();
 
 /**
@@ -46,9 +46,95 @@ const buyProduct = async(userId, productId, quantity) => {
     });
 };
 
+/**
+ * 결제 취소
+ */
+const cancelPaymentProduct = async(orderId, productId) => {
+    await prisma.orderItem.delete({
+        where:{
+            pay_id_product_id: {
+                pay_id: orderId,
+                product_id: productId
+            }
+        }
+    });
+    
+    // 남은 상품이 없으면 order 삭제
+    const remainingItems = await prisma.orderItem.findMany({
+        where: { pay_id: orderId }
+    });
+
+    if (remainingItems.length === 0) {
+        await prisma.order.delete({ where: { id: orderId } });
+    }
+};
+
+
+/**
+ * 사용자가 구매한 상품 목록
+ */
+const getPurchasedProducts = async(userId) => {
+    const purchasedProducts = await prisma.order.findMany({
+        where: {
+            buyer: userId,
+            status: OrderStatus.SUCCEEDED
+        }
+    });
+    return purchasedProducts;
+};
+
+
+/**
+ * 장바구니에 상품 등록
+ */
+const addProductCart = async(userId, productId, quantity) => {
+    await prisma.cart.create({
+        data:{
+            owner: userId,
+            cartItem: productId,
+            quantity: quantity
+        }
+    });
+};
+
+/**
+ * 장바구니 상품 삭제
+ */
+const deleteProductCart = async(userId, productId) => {
+    await prisma.cart.delete({
+        where: {
+            userId_productId: {
+                userId,
+                productId
+            }
+        }
+    })
+};
+
+/**
+ * 장바구니의 상품 수량 변경
+ */
+const updateProductQuantityCart = async(userId, productId, quantity) => {
+    await prisma.cart.update({
+        where:{
+            userId_productId: {
+                userId,
+                productId
+            }
+        },
+        data: {
+            quantity
+        }
+    })
+};
 
 module.exports = {
     getCategoryProduct,
     getProductInfo,
-    buyProduct
+    buyProduct,
+    getPurchasedProducts,
+    addProductCart,
+    deleteProductCart,
+    updateProductQuantityCart,
+    cancelPaymentProduct
 }
